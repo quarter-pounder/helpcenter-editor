01dd382a3c62006687a3cf7887cbd890eda24845
drawerwrangler <char.js.lin@gmail.com>
2025-11-26 13:52:55 +0900

    Switch to static adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,21 +1,21 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
+export default {
 	preprocess: vitePreprocess(),
 
 	kit: {
 		adapter: adapter(),
+		prerender: {
+			entries: ['*']
+		},
 		alias: {
 			$lib: 'src/lib',
 			$components: 'src/lib/components',
 			$api: 'src/lib/api',
 			$types: 'src/lib/types',
 			$stores: 'src/lib/stores',
-			$utils: 'src/lib/utils',
-		},
-	},
-};
-
-export default config;
+			$utils: 'src/lib/utils'
+		}
+	}
+}
